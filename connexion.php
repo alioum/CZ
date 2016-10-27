542341606780aa75c35a784956bfc630e068f570
alioum <alexdarmon06@gmail.com>
2016-10-28 00:19:33 +0200

    Delete connexion.php

--- connexion.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$user = "root";
-$pass = "";
-$db = "czdb";
-// on se connecte à MySQL 
-//$db = new PDO('mysql:host=localhost;dbname=czdb;charset=utf8', $user, $pass);
-$mysqli = mysqli_connect("127.0.0.1", $user, $pass, $db);
